ff3243bbdb602b72cb54df8066968aee0530c07a
Ricardo Diaz <Ricardo.Diaz@endava.com>
2020-01-13 14:48:25 -0500

    useEffect for other query API

--- src/App.js
+++ src/App.js
@@ -8,29 +8,43 @@ function App() {
   const [lyric, addLyric] = useState([]);
   const [information, addInformation] = useState({});
 
-  const queryAPILyric =  async search => {
-    const {artist , song} = search;
+  const queryAPILyric = async search => {
+    const { artist, song } = search;
     const url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
 
     const result = await axios(url);
+    addArtist(artist);
     addLyric(result.data.lyrics);
   }
 
+  const queryAPIInfo = async () => {
+    const url = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
+    const result = await axios(url);
+
+    addInformation(result.data.artists[0]);
+
+  }
+
+  useEffect(
+    () => {
+      queryAPIInfo();
+    }, [artist]
+  )
   return (
     <Fragment>
       <Form
         queryAPILyric={queryAPILyric}
       />
-      <div className = "container mt-5">
-        <div className = "row">
-          <div className = "col-md-6">
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
 
           </div>
-          <div className = "col-md-6">
+          <div className="col-md-6">
             <Song
-            lyric = {lyric}
+              lyric={lyric}
             />
-            </div>
+          </div>
         </div>
       </div>
     </Fragment>
