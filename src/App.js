828716d055353f21e770b2c55891a5905e84b06a
Ricardo Diaz <Ricardo.Diaz@endava.com>
2020-01-13 11:26:32 -0500

    consulting lyric API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import Form from './components/Form';
-
+import axios from 'axios';
 function App() {
 
   const [artist, addArtist] = useState('');
   const [lyric, addLyric] = useState([]);
   const [information, addInformation] = useState({});
 
-  const queryAPILyric = search =>{
-        console.log(search)
+  const queryAPILyric =  async search => {
+    const {artist , song} = search;
+    const url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+
+    const result = await axios(url);
+    addLyric(result.data.lyrics);
   }
 
   return (
     <Fragment>
       <Form
-      queryAPILyric ={queryAPILyric}
+        queryAPILyric={queryAPILyric}
       />
     </Fragment>
   );
